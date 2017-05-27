c661e27196fbacdb387acc467b7ca0edb1297c45
John Krowas <jkrowas@gmail.com>
2017-05-27 06:26:30 -0700

    cosmetics

--- pge.py
+++ pge.py
@@ -15,6 +15,8 @@ bokeh.charts.defaults.legend = False
 
 parser = argparse.ArgumentParser(description="Process PG\&E Data")
 parser.add_argument('zipfile')
+parser.add_argument('--temps', dest='tfile', help='NWS temperature record')
+
 args = parser.parse_args()
 
 if os.getenv('temp') is None:
@@ -25,6 +27,8 @@ tempdir = os.getenv('temp') + '\\'
 
 gasfile = ''
 elecfile = ''
+tfile = args.tfile
+
 
 with zipfile.ZipFile(args.zipfile, 'r') as myzip:
     zi = myzip.infolist()
@@ -46,14 +50,29 @@ def truncRes(df):
     resampled = renamed.resample("1W").sum()
     return resampled
 
+if tfile is not None:
+    tdf = pandas.read_csv(tfile, index_col=2, parse_dates=True)
+    tdf['Current Year TMIN (lagged)'] = tdf['TMIN'].shift(-365, freq="D")
+    trunctdf = tdf[:tdf.index.max() - pandas.to_timedelta('365 days')]
+    trunctdf = trunctdf.resample("1W").mean()
+
+
 if gasfile != '':
     gasdf = pandas.read_csv(tempdir+gasfile, skiprows=5, index_col=1, parse_dates=True)
     gastochart = truncRes(gasdf)
+    try:
+        trunctdf
+    except:
+        pass
+    else:
+        gastochart['TMIN'] = trunctdf['TMIN']
+        gastochart['Current Year TMIN (lagged)'] = trunctdf['Current Year TMIN (lagged)']
     gaschart = bokeh.charts.TimeSeries(gastochart, title="Gas Usage",  xlabel="Date", legend=True,ylabel="Therms")
 
 if elecfile != '':
     elecdf = pandas.read_csv(tempdir+elecfile, skiprows=5, index_col=1, parse_dates=True)
     electochart = truncRes(elecdf)
     elecchart = bokeh.charts.TimeSeries(electochart, title="Electricity Usage", legend=True, xlabel="Date", ylabel="kWh")
+
 
 show(column(gaschart, elecchart))
